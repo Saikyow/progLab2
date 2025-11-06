b9ba5c1193323556dd0b72c5442e2866b34d72fd
Pogorelchenko Fedor <pogorelchenko.fedor@gmail.ru>
2025-11-06 16:06:07 +0300

    added pokemons to battle

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
-import mypokemons.Carbink;
-import mypokemons.Deino;
-import mypokemons.Hydgion;
+import mypokemons.*;
 import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Pokemon;
 
 public class Main {
     public static void main(String[] args) {
        Battle b =  new Battle();
-        Pokemon p1 = new Hydgion("gwgw", 1);
+        Pokemon p1 = new Hydgion("ркур", 1);
         Pokemon p2 = new Deino("Хcs,", 1);
+        Pokemon p3 = new Carbink("grnjh", 1);
+        Pokemon p4 = new Zweilous("53jh", 1);
+        Pokemon p5 = new Phantump("62", 1);
+        Pokemon p6 = new Hydgion("g72jh", 1);
+
         b.addAlly(p1);
-        b.addFoe(p2);
+        b.addAlly(p2);
+        b.addAlly(p3);
+        b.addFoe(p4);
+        b.addFoe(p5);
+        b.addFoe(p6);
         b.go();
     }
 }
